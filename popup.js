fa654b429e6529a48b8ea4d87c4a8a5537a1e4ca
Aremu Smog <aremuoluwagbamila@gmail.com>
2023-10-11 20:07:18 +0100

    feat: load iframe via javascript

--- popup.js
+++ popup.js
@@ -1,1 +1,10 @@
+window.addEventListener("load", e => {
+	// console.log("Dom content is loaded")
+	// alert("Document loaded")
+	const body = document.querySelector("body")
+	const iframe = document.createElement("iframe")
+	iframe.src = "https://fast.com/"
+	iframe.setAttribute("id", "fast-container")
 
+	body.append(iframe)
+})
